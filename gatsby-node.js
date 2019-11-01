049641ebc69104bf1cc5265684b1c59d0b150dfc
Dallas <dpeters@americanbible.org>
2019-11-01 10:03:56 -0500

    trying to get netlify to build

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,19 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ stage, rules, loaders, actions }) => {
+  switch (stage) {
+    case "build-html":
+      actions.setWebpackConfig({
+        module: {
+          rules: [
+            {
+              test: /react-bigpicture/,
+              use: [loaders.null()],
+            },
+          ],
+        },
+      })
+      break
+  }
+}
