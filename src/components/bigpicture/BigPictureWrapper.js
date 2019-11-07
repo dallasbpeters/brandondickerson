7f03e048857ad74361ceaaed5ad0d11ed20b43a4
Bryce Allison <bryceallison@gmail.com>
2019-11-07 12:20:32 -0600

    Conditionally require bigpicture

--- src/components/bigpicture/BigPictureWrapper.js
+++ src/components/bigpicture/BigPictureWrapper.js
@@ -1,6 +1,6 @@
 import React, { useRef } from "react"
 import PropTypes from "prop-types"
-import BigPicture from "bigpicture"
+const BigPicture = typeof window !== `undefined` ? require("bigpicture") : null
 
 const BigPictureWrapper = ({
   type = "image",
